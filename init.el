5d36de7b53df389b08ca157a9341fb03fb5841e1
Jayme Calixto <jaymecalixto@gmail.com>
2019-10-28 09:50:54 -0300

    nim

--- init.el
+++ init.el
@@ -124,6 +124,10 @@
 	    (setq indent-tabs-mode nil)))
 
 
+;; nim
+;; (use-package nim-mode
+;;  :ensure t)
+
 (custom-set-variables
  ;; custom-set-variables was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
