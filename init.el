8ad521cf2db090b28cbd8f0ed20248d358a6550e
Jayme Calixto <jaymecalixto@gmail.com>
2020-01-11 22:07:01 -0300

    company backend for anaconda python-mode

--- init.el
+++ init.el
@@ -280,6 +280,14 @@
   (add-hook 'python-mode-hook 'anaconda-eldoc-mode)
   )
 
+(use-package company-anaconda
+  :ensure t
+  :after company
+  :config
+  (add-to-list 'company-backends 'company-anaconda)
+  (add-hook 'python-mode-hook 'company-mode)
+  )
+
 (use-package origami
   :ensure t
   )
@@ -333,9 +341,10 @@
  ;; If you edit it by hand, you could mess it up, so be careful.
  ;; Your init file should contain only one such instance.
  ;; If there is more than one, they won't work right.
+ '(dart-format-on-save t t)
  '(package-selected-packages
    (quote
-    (gnu-elpa-keyring-update transient magit markdown-mode yaml-mode all-the-icons general which-key helm doom-themes evil use-package))))
+    (company-anaconda gnu-elpa-keyring-update transient magit markdown-mode yaml-mode all-the-icons general which-key helm doom-themes evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
