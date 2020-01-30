455037f7f5a8b2aaf8931e208cc4642ef0b134e9
jayme <jayme@esolution.inf.br>
2020-01-30 08:31:28 -0300

    srcery theme

--- init.el
+++ init.el
@@ -437,7 +437,7 @@
  '(dart-format-on-save t t)
  '(package-selected-packages
    (quote
-    (blacken python-mode elpy workgroups gnu-elpa-keyring-update transient magit markdown-mode yaml-mode all-the-icons general which-key helm doom-themes evil use-package))))
+    (srcery-theme blacken python-mode elpy workgroups gnu-elpa-keyring-update transient magit markdown-mode yaml-mode all-the-icons general which-key helm doom-themes evil use-package))))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
