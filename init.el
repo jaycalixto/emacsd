d762f26a27b132a6c0b1df8f5e8fc6f57aecb69a
Jayme Calixto <jayme@esolution.inf.br>
2020-05-05 15:49:58 -0300

    auto update packages increased to 30 days instead of 4

--- init.el
+++ init.el
@@ -263,7 +263,7 @@
   :ensure t
   :config
   (setq auto-package-update-delete-old-versions t
-	auto-package-update-interval 4)
+	auto-package-update-interval 30)
   (auto-package-update-maybe))
 
 
