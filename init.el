b86963faed7ac4f39733881918a89cdab2c1aca1
Jayme Calixto <jaymecalixto@gmail.com>
2021-02-02 22:06:29 -0300

    elixir

--- init.el
+++ init.el
@@ -505,6 +505,15 @@
   )
 
 ;; elixir
+(use-package elixir-mode
+  :ensure t
+  :defer t
+  :config
+  (add-hook 'elixir-mode-hook 'alchemist-mode)
+  (add-hook 'elixir-mode-hook 'company-mode)
+  (add-hook 'elixir-mode-hook 'smartparens-mode)
+  (add-hook 'elixir-mode-hook (lambda () (add-hook 'before-save-hook 'elixir-format nil t))))
+
 (use-package alchemist
   :ensure t
   :defer t)
