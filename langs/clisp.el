5d01ed200ec080c83b96fa33b907f70a3d402155
Jayme Calixto <jaymecalixto@gmail.com>
2021-06-13 11:33:06 -0300

    lisp keybindings

--- langs/clisp.el
+++ langs/clisp.el
@@ -6,7 +6,11 @@
     (add-hook 'lisp-mode-hook 'company-mode)
     (add-hook 'lisp-mode-hook 'show-paren-mode)
     (add-hook 'lisp-mode-hook 'electric-pair-mode)
-    (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode))
+    (add-hook 'lisp-mode-hook 'rainbow-delimiters-mode)
+    (general-evil-define-key 'normal lisp-mode-map
+      :prefix "SPC"
+      "cs" '(slime :which-key "run slime")
+      "cc" '(slime-eval-buffer :which-key "eval buffer")))
   :config
   (progn
     (load (expand-file-name "~/quicklisp/slime-helper.el"))
