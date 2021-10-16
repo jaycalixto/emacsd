2ad242e3de89f9c0295cc3addeb3309cc62d0573
Jayme Calixto <jaymecalixto@gmail.com>
2021-10-15 22:42:17 -0300

    better c mode

--- langs/c.el
+++ langs/c.el
@@ -3,7 +3,23 @@
   :defer t
   :config
   (progn
-    (add-hook 'c-mode-hook #'company-mode)))
+    (require 'semantic)
+    (global-semanticdb-minor-mode 1)
+    (global-semantic-idle-scheduler-mode 1)
+    (add-hook 'c-mode-hook #'semantic-mode)
+    (add-hook 'c-mode-hook #'electric-pair-mode)
+    (add-hook 'c-mode-hook #'company-mode)
+    (add-hook 'c-mode-hook
+              (lambda() (setq-local company-backends
+                                    '(company-semantic
+                                      company-dabbrev-code
+                                      company-keywords
+                                      company-files
+                                      company-dabbrev))))
+    (setq c-basic-offset 4)
+    (setq c-default-style '((java-mode . "java")
+                            (awk-mode . "awk")
+                            (other . "linux")))))
 
 (use-package cmake-mode
   :ensure t
