4538df8e7631bc83c3b58f63eb395923874dca0a
Jayme Calixto <jaymecalixto@gmail.com>
2022-04-03 22:14:07 -0300

    zig mode remap

--- langs/zig.el
+++ langs/zig.el
@@ -5,4 +5,13 @@
   (progn
     (add-hook 'zig-mode-hook #'electric-pair-mode)
     (add-hook 'zig-mode-hook #'rainbow-delimiters-mode)
-    (add-hook 'zig-mode-hook #'company-mode)))
+    (add-hook 'zig-mode-hook #'company-mode))
+  :general
+  (:keymaps 'zig-mode-map
+            :prefix "SPC"
+            :states '(normal visual)
+            "cb" '(zig-compile :which-key "zig compile")
+            "cf" '(zig-format-buffer :which-key "zig format buffer")
+            "ct" '(zig-test-buffer :which-key "zig test buffer")
+            "cp" '(zig-toggle-format-on-save :which-key "zig toggle format on save")
+            "cr" '(zig-run :which-key "zig run")))
