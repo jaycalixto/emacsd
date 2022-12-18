97fb5a2039e22a679bcc0290c6ab4ab87b0c8e18
Jayme Calixto <jaymecalixto@gmail.com>
2022-12-18 19:21:59 -0300

    typescript tsx files

--- langs/typescript.el
+++ langs/typescript.el
@@ -21,3 +21,11 @@
     (add-hook 'typescript-mode-hook #'rainbow-delimiters-mode)
     (add-hook 'typescript-mode-hook #'electric-pair-mode)
     (add-hook 'before-save-hook 'tide-format-before-save)))
+
+(require 'web-mode)
+(add-to-list 'auto-mode-alist '("\\.tsx\\'" . web-mode))
+(add-hook 'web-mode-hook
+          (lambda ()
+            (when (string-equal "tsx" (file-name-extension buffer-file-name))
+              (setup-tide-mode))))
+;; (flycheck-add-mode 'typescript-tslint 'web-mode)
