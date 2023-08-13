0d23b72093925a5bfda2919a5085db635bdbb7e6
Jayme Calixto <jaymecalixto@gmail.com>
2023-08-13 20:41:28 -0300

    new theme

--- themes.el
+++ themes.el
@@ -1,4 +1,6 @@
 (add-to-list
  'custom-theme-load-path
  (expand-file-name "themes" user-emacs-directory))
-(load-theme 'atom-one-dark t)
+;; (load-theme 'rebecca t)
+;; (load-theme 'atom-one-dark t)
+(load-theme 'srcery t)
